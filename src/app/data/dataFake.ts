2f00110fce3d21c56378fcbaa2873f6f390c8de8
Fred-Rezende <raizen.fred@gmail.com>
2024-10-14 15:39:25 -0300

    content created

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "Rurouni Kenshin",
+    "description": "Durante a agitação da Era Bakumatsu, havia um guerreiro Imperialista temido como o Battousai, o Retalhador. No entanto, com a chegada da nova era, o Battosai desapareceu dos olhos do público, deixando para trás apenas sua lenda como o mais forte guerreiro Revolucionário.",
+    "photoCover":"https://imgsrv.crunchyroll.com/cdn-cgi/image/fit=contain,format=auto,quality=85,width=1200,height=675/catalog/crunchyroll/8c989998711d1f0ee8923fd96e023e96.jpg"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "One Piece",
+    "description": "Embarque em uma jornada com One Piece. A épica série de anime criada pelo renomado Eiichiro Oda é um fenômeno, cativando corações de fãs de várias gerações ao longo de seus 25 anos de existência. Esta aventura em alto-mar é repleta de amizades inabaláveis, batalhas pela liberdade e a incessante busca pelos sonhos. Junte-se a Monkey D. Luffy e sua tripulação de piratas enquanto eles descobrem o verdadeiro significado de poder e justiça nesta grande era dos piratas.",
+    "photoCover":"https://imgsrv.crunchyroll.com/cdn-cgi/image/fit=contain,format=auto,quality=85,width=1200,height=675/catalog/crunchyroll/a249096c7812deb8c3c2c907173f3774.jpg"
+  },
+  {
+    "id":"3",
+    "title": "DAN DA DAN",
+    "description": "Esta é uma história sobre Momo, uma garota do ensino médio que vem de uma família de médiuns espirituais, e seu colega de classe Okarun, um garoto fanático pelo ocultismo. Depois que Momo resgata Okarun de uns valentões, eles começam a conversar... No entanto, surge uma discussão entre eles, já que Momo acredita em fantasmas, mas nega a existência de alienígenas, e Okarun acredita em alienígenas, mas nega a existência de fantasmas.",
+    "photoCover":"https://imgsrv.crunchyroll.com/cdn-cgi/image/fit=contain,format=auto,quality=85,width=1200,height=675/catalog/crunchyroll/fa62dd1fc7a9bc0b587f36f53bf572c1.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Bungo Stray Dogs",
+    "description": "Expulso do orfanato e prestes a morrer de fome, Atsushi Nakajima conhece alguns homens bastante estranhos. Um deles, Osamu Dazai, é um suicida tentando se afogar em plena luz do dia. Outro, chamado Doppo Kunikida, fica parado, folheando nervosamente seu bloco de anotações. Ambos são membros da Agência de Detetives Armados, e se dizem responsáveis por solucionar casos que nem a polícia e o exército ousam tocar. Atsushi acaba acompanhando-os em uma missão para eliminar um tigre comedor de gente que está aterrorizando a população.",
+    "photoCover":"https://imgsrv.crunchyroll.com/cdn-cgi/image/fit=contain,format=auto,quality=85,width=1200,height=675/catalog/crunchyroll/8f5fce5262796491d3d9332b0e5551bf.jpg"
   }
 ]
